22634738bdb458d8bbad020b90c9e88133cf72a8
dk-Gk <idkin19@gmail.com>
2022-06-18 22:30:26 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,10 +6,10 @@ def pascal_triangle(n):
     """returns a list of lists of integers representing Pascal’s triangle"""
     list_T = []
     if(n <= 0):
-        return tri
+        return list_T
     for i in range(n):
         list_T.append([])
-        list_T[i].append(1)
+        """list_T[i].append(1)"""
         for j in range(1, i):
             list_T[i].append(list_T[i - 1][j - 1] + list_T[i - 1][j])
         if(n != 0):
