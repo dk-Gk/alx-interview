f71f2caef25d8608f5871beba641d25b60304cd6
dk-Gk <idkin19@gmail.com>
2022-08-11 23:21:28 +0000

    Starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,26 @@
 
 const request = require('request');
 if (process.argv.length === 3) {
-    const myArgs = process.argv.slice(2);
-    const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
-    const options = { json: true };
+  const myArgs = process.argv.slice(2);
+  const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+  const options = { json: true };
 
-    request(url, options, async function (error, res, body) {
-	if (error) {
-	    console.log(error);
-	} else {
-	    for (const char of body.characters) {
-		const ret = () => {
-		    return new Promise((resolve, reject) => {
-			request(char, options, function (error, res, body) {
-			    if (error) { console.log(error); } else {
-				resolve(body.name);
-			    }
-			});
-		    });
-		};
-		console.log(await ret());
-	    }
-	}
-    });
+  request(url, options, async function (error, res, body) {
+      if (error) {
+	  console.log(error);
+      } else {
+	  for (const char of body.characters) {
+	      const ret = () => {
+		  return new Promise((resolve, reject) => {
+		      request(char, options, function (error, res, body) {
+			  if (error) { console.log(error); } else {
+			      resolve(body.name);
+			  }
+		      });
+		  });
+	      };
+	      console.log(await ret());
+	  }
+      }
+  });
 }
