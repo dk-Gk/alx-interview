3aee66a0c79df957b9f40c1c8fab3b7c92a0b8e1
dk-Gk <idkin19@gmail.com>
2022-08-11 23:23:30 +0000

    Starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,21 +7,21 @@ if (process.argv.length === 3) {
   const options = { json: true };
 
   request(url, options, async function (error, res, body) {
-      if (error) {
-	  console.log(error);
-      } else {
-	  for (const char of body.characters) {
-	      const ret = () => {
-		  return new Promise((resolve, reject) => {
-		      request(char, options, function (error, res, body) {
-			  if (error) { console.log(error); } else {
-			      resolve(body.name);
-			  }
-		      });
-		  });
-	      };
-	      console.log(await ret());
-	  }
-      }
+    if (error) {
+	console.log(error);
+    } else {
+	for (const char of body.characters) {
+	    const ret = () => {
+		return new Promise((resolve, reject) => {
+		    request(char, options, function (error, res, body) {
+			if (error) { console.log(error); } else {
+			    resolve(body.name);
+			}
+		    });
+		});
+	    };
+	    console.log(await ret());
+	}
+    }
   });
 }
