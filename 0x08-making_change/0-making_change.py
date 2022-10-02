dc62723cca7b62a9c355b5ff58e5e20b4f05d57b
dk-Gk <idkin19@gmail.com>
2022-10-02 15:10:14 +0000

    Fixed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,21 +7,15 @@ def makeChange(coins, total):
     determine the fewest number of coins needed to
     meet a given amount total
     """
-    if (type(total) is not int or type(coins) is not list):
-        return -1
-    if total <= 0:
+    if total < 1:
         return 0
-    try:
-        Min = [float('inf') for i in range(total+1)]
-        Min[0] = 0
-        for i in range(1, total+1):
-            for j in range(len(coins)):
-                if Min[i - coins[j]] + 1 < Min[i]:
-                    Min[i] = Min[i - coins[j]] + 1
-
-        if Min[total] != float('inf'):
-            return Min[total]
-        else:
+    change = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
+        if total == 0:
+            return change
+        if total != 0:
             return -1
-    except Exception:
-        return -1
