a89693cfa034f651326eff2d6d6acef072bd4878
dk-Gk <idkin19@gmail.com>
2023-06-16 17:07:13 +0000

    fix error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,8 +14,16 @@ def makeChange(coins, total):
 
     change = [[max_value] * (total + 1) for _ in range(num_coins + 1)]
     for i in range(num_coins + 1):
-        dp[i][0] = 0
+        change[i][0] = 0
     for i in range(1, num_coins + 1):
         coin_value = coins[i - 1]
         for j in range(1, total + 1):
-            
+            if coin_value > j:
+                change[i][j] = change[i - 1][j]
+            else:
+                change[i][j] = min(change[i - 1][j], 1 + change[i][j - coin_value])
+    
+    if change[num_coins][total] != max_value:
+        return change[num_coins][total]
+    else:
+        return -1
